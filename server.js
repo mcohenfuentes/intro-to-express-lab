c350404ec5a25753f34aa81d76ddcca82867a0dd
Mica CF <mcohenfuentes@gmail.com>
2025-02-11 20:31:24 -0500

    exercise 4 incomplete

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express');
 
 const app = express();
 
+
+//1. Be Polite, Greet the User
 app.get('/greetings/:username', (req, res) => {
 
     res.send(`'<h1>Greetings ${req.params.username}</h1>`)
 })
 
-
+//2. Rolling the Dice
 app.get('/roll/:numberParameter', (req, res) => {
    const randomNumber = Math.ceil(Math.random() * req.params.numberParameter)
    
@@ -19,6 +21,7 @@ app.get('/roll/:numberParameter', (req, res) => {
    
 })
 
+//3. I Want THAT One!
 const collectibles = [
     { name: 'shiny ball', price: 5.95 },
     { name: 'autographed picture of a dog', price: 10 },
@@ -34,6 +37,27 @@ app.get('/collectibles/:index', (req, res) => {
         res.send(`So you want the ${item.name}? For ${item.price}, it can be yours!`);
     }
 });
+
+//4. Filter Shoes by Query Parameters
+
+const shoes = [
+    { name: "Birkenstocks", price: 50, type: "sandal" },
+    { name: "Air Jordans", price: 500, type: "sneaker" },
+    { name: "Air Mahomeses", price: 501, type: "sneaker" },
+    { name: "Utility Boots", price: 20, type: "boot" },
+    { name: "Velcro Sandals", price: 15, type: "sandal" },
+    { name: "Jet Boots", price: 1000, type: "boot" },
+    { name: "Fifty-Inch Heels", price: 175, type: "heel" }
+];
+
+
+
+app.get('/shoes', (req, res) => {
+    
+});
+
+
+
 
 
 
